11d0d99b0ce73431733ff911e723518707a91f27
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-02-27 12:02:18 +0100

    Doned Page Show Simulations and Doned Footer Main Page, title done

--- Pages/Simulations.cshtml.cs
+++ Pages/Simulations.cshtml.cs
@@ -11,19 +11,34 @@ namespace t4_pr1_LlucVelazquez.Pages
 {
     public class SimulationsModel : PageModel
     {
-		private const string CsvFilePath = @"ModelData\simulacions_energia.csv";
-		public List<Simulation> Products { get; set; } = new List<Simulation>();
+		public List<Simulation> Simulations { get; set; } = new List<Simulation>();
 		public void OnGet()
         {
-			using StreamReader reader = new StreamReader(CsvFilePath);
-			using CsvReader csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+			string CsvFilePath = @"ModelData\simulacions_energia.csv";
 
-			var records = csvReader.GetRecords<Simulation>();
-
-			foreach (var record in records)
+			string[] lines = FileWorking.File.ReadAllLines(CsvFilePath);
+			foreach (string line in lines)
 			{
-				Console.WriteLine($"{record.Date}, {record.TypeSim}, {record.Valor}, {record.Rati}, {record.EnergyGen}, {record.Cost}, {record.Preu}, {record.CostTotal}, {record.PreuTotal}");
+				string[] parts = line.Split(',');
+				if(parts.Length == 9)
+				{
+					Simulation simulation = new Simulation();
+					simulation.Date = DateTime.Parse(parts[0]);
+					simulation.TypeSim = parts[1];
+					simulation.Valor = float.Parse(parts[2]);
+					simulation.Rati = float.Parse(parts[3]);
+					simulation.EnergyGen = float.Parse(parts[4]);
+					simulation.Cost = decimal.Parse(parts[5]);
+					simulation.Preu = decimal.Parse(parts[6]);
+					simulation.CostTotal = decimal.Parse(parts[7]);
+					simulation.PreuTotal = decimal.Parse(parts[8],CultureInfo.InvariantCulture);
+					Simulations.Add(simulation);
+				}
 			}
 		}
     }
 }
+
+/*
+ simulacions_energia.csv header : Date,TypeSim,Valor,Rati,EnergyGen,Cost,Preu,CostTotal,PreuTotal
+*/
