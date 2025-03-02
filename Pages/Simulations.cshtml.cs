31860b4c93bc194cb5b142469d026c70f994efa7
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-03-02 18:55:46 +0100

    Calc Energy Generated doned, menu Type Energy Doned, miss show error missage missing items in file

--- Pages/Simulations.cshtml.cs
+++ Pages/Simulations.cshtml.cs
@@ -11,30 +11,43 @@ namespace t4_pr1_LlucVelazquez.Pages
 {
     public class SimulationsModel : PageModel
     {
+		public string FileErrorMessage;
 		public List<Simulation> Simulations { get; set; } = new List<Simulation>();
 		public void OnGet()
         {
 			string CsvFilePath = @"ModelData\simulacions_energia.csv";
 
-			string[] lines = FileWorking.File.ReadAllLines(CsvFilePath);
-			foreach (string line in lines)
+			if (FileWorking.File.Exists(CsvFilePath))
 			{
-				string[] parts = line.Split(',');
-				if(parts.Length == 9)
-				{
-					Simulation simulation = new Simulation();
-					simulation.Date = DateTime.Parse(parts[0]);
-					simulation.TypeSim = parts[1];
-					simulation.Valor = double.Parse(parts[2]);
-					simulation.Rati = double.Parse(parts[3]);
-					simulation.EnergyGen = double.Parse(parts[4]);
-					simulation.Cost = decimal.Parse(parts[5]);
-					simulation.Preu = decimal.Parse(parts[6]);
-					simulation.CostTotal = decimal.Parse(parts[7]);
-					simulation.PreuTotal = decimal.Parse(parts[8],CultureInfo.InvariantCulture);
-					Simulations.Add(simulation);
-				}
-			}
+                string[] lines = FileWorking.File.ReadAllLines(CsvFilePath);
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(',');
+                    if (parts.Length == 9)
+                    {
+                        Simulation simulation = new Simulation();
+                        simulation.Date = DateTime.Parse(parts[0]);
+                        simulation.TypeSim = parts[1];
+                        simulation.Valor = double.Parse(parts[2]);
+                        simulation.Rati = double.Parse(parts[3]);
+                        simulation.EnergyGen = double.Parse(parts[4]);
+                        simulation.Cost = decimal.Parse(parts[5]);
+                        simulation.Preu = decimal.Parse(parts[6]);
+                        simulation.CostTotal = decimal.Parse(parts[7]);
+                        simulation.PreuTotal = decimal.Parse(parts[8], CultureInfo.InvariantCulture);
+                        Simulations.Add(simulation);
+                    }
+                    else
+                    {
+                        FileErrorMessage = "Error de carrega delsatributs d'un producte";
+                    }
+                }
+            }
+            else
+            {
+                FileErrorMessage = "Error de carrega de dades";
+            }
+			
 
 			/*if (System.IO.File.Exists(CsvFilePath))
 			{
