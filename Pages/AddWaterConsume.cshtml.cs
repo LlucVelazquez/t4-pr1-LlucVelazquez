7715f66ae9d3cc762dbe74f844dd23e6e937903f
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-03-06 13:49:57 +0100

    Doned Add Water Consume

--- Pages/AddWaterConsume.cshtml.cs
+++ Pages/AddWaterConsume.cshtml.cs
@@ -20,44 +20,6 @@ namespace t4_pr1_LlucVelazquez.Pages
             {
                 return Page();
             }
-			/*XmlDocument xmlDocument = new XmlDocument();
-			XmlElement consumesElement = xmlDocument.CreateElement("Cosa");
-            xmlDocument.AppendChild(consumesElement);
-
-            XmlElement waterElement = xmlDocument.CreateElement("Consume");
-            consumesElement.AppendChild(waterElement);
-
-			AddChildElementInt(xmlDocument, waterElement, "Year", NewWaterConsume.Year);
-			AddChildElementInt(xmlDocument, waterElement, "Code", NewWaterConsume.Code);
-			AddChildElement(xmlDocument, waterElement, "Region", NewWaterConsume.Region);
-			AddChildElementInt(xmlDocument, waterElement, "Population", NewWaterConsume.Population);
-			AddChildElementInt(xmlDocument, waterElement, "DomesticNetwork", NewWaterConsume.DomesticNetwork);
-			AddChildElementInt(xmlDocument, waterElement, "EconomicActOwnSource", NewWaterConsume.EconomicActOwnSource);
-			AddChildElementInt(xmlDocument, waterElement, "Total", NewWaterConsume.Total);
-			AddChildElementDecimal(xmlDocument, waterElement, "HouseholdConsumCapita", NewWaterConsume.HouseholdConsumCapita);
-
-			string filePath = @"ModelData\consum_aigua_cat_per_comarques.xml";
-			xmlDocument.Save(filePath);
-
-
-			static void AddChildElement(XmlDocument xmlDoc, XmlElement parentElement, string elementName, string value)
-			{
-				XmlElement childElement = xmlDoc.CreateElement(elementName);
-				childElement.InnerText = value;
-				parentElement.AppendChild(childElement);
-			}
-			static void AddChildElementInt(XmlDocument xmlDoc, XmlElement parentElement, string elementName, int value)
-			{
-				XmlElement childElement = xmlDoc.CreateElement(elementName);
-				childElement.InnerText = value.ToString();
-				parentElement.AppendChild(childElement);
-			}
-			static void AddChildElementDecimal(XmlDocument xmlDoc, XmlElement parentElement, string elementName, decimal value)
-			{
-				XmlElement childElement = xmlDoc.CreateElement(elementName);
-				childElement.InnerText = value.ToString();
-				parentElement.AppendChild(childElement);
-			}*/
 			string filePath = @"ModelData\consum_aigua_cat_per_comarques.xml";
 			XDocument doc = XDocument.Load(filePath);
 			XElement element = doc.Element("Consumes");
@@ -72,18 +34,6 @@ namespace t4_pr1_LlucVelazquez.Pages
 				   new XElement("HouseholdConsumCapita", NewWaterConsume.HouseholdConsumCapita)
 				   ));
 			   doc.Save(filePath); 
-			/*
-			   string filePath = @"ModelData\consum_aigua_cat_per_comarques.xml";
-			   string? waterConsumeS = $"{NewWaterConsume.Year},{NewWaterConsume.Code},{NewWaterConsume.Region},{NewWaterConsume.Population}" +
-				   $",{NewWaterConsume.DomesticNetwork},{NewWaterConsume.EconomicActOwnSource},{NewWaterConsume.Total},{NewWaterConsume.HouseholdConsumCapita}";
-			   if (System.IO.File.Exists(filePath))
-			   {
-				   System.IO.File.AppendAllText(filePath, waterConsumeS + Environment.NewLine);
-			   }
-			   else
-			   {
-				   Debug.WriteLine("No trobo el fitxer");
-			   }*/
 			return RedirectToPage("WaterConsume");
         }
     }
