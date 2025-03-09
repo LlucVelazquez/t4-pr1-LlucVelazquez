09f9b96c5683330e64237ca6af3f4c1efd47e9a6
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-03-09 14:52:16 +0100

    Doned statistic Gas consume more than 100 and statistic Production average for year

--- Pages/EnergyIndicators.cshtml.cs
+++ Pages/EnergyIndicators.cshtml.cs
@@ -13,6 +13,9 @@ namespace t4_pr1_LlucVelazquez.Pages
         public string FileErrorMessage;
         public List<EnergyIndicator> EnergyIndicators { get; set; } = new();
         public List<EnergyIndicator> NetProductionAbove3000 => EnergyIndicators.Where(i => i.CDEEBC_ProdNeta > 3000).OrderBy(i => i.CDEEBC_ProdNeta).ToList();
+        public List<EnergyIndicator> GasConsumMore100 => EnergyIndicators.Where(i => i.CCAC_GasolinaAuto > 100).OrderByDescending(i => i.CCAC_GasolinaAuto).ToList();
+        public List<(int any, decimal mitjanaProduccioNeta)> ProductionAverageForYear => EnergyIndicators.GroupBy(i => i.Data.Year)
+                     .Select(g => (any: g.Key, mitjanaProduccioNeta: g.Average(i => i.CDEEBC_ProdNeta))).OrderBy(r => r.any).ToList();
         public void OnGet()
         {
             string CsvFilePath = @"ModelData\indicadors_energetics_cat.csv";
